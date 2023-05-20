b08026150ccad77d77b4e5470f8dfdbe42e92044
0618 <o.6180339@gmail.com>
2023-05-19 18:17:28 -0700

    test platform-tools/adb logcat -c

--- .github/scripts/build-android.sh
+++ .github/scripts/build-android.sh
@@ -14,11 +14,11 @@ COLOR_END="\033[0m"
 # To resolve "error Command failed: /Users/runner/Library/Android/sdk/platform-tools/adb logcat -c"
 # https://stackoverflow.com/questions/63617294/error-while-running-android-build-on-react-native
 # https://stackoverflow.com/questions/55677874/failed-to-launch-emulator-error-emulator-didnt-connect-within-60-seconds
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+# export ANDROID_HOME=$HOME/Library/Android/sdk
+# export PATH=$PATH:$ANDROID_HOME/emulator
+# export PATH=$PATH:$ANDROID_HOME/tools
+# export PATH=$PATH:$ANDROID_HOME/tools/bin
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 echo -e "${BLUE_BOLD}cd build-system-tests/mega-apps/${MEGA_APP_NAME}${COLOR_END}"
 cd build-system-tests/mega-apps/${MEGA_APP_NAME}
